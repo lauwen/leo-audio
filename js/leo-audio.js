2986f5aaaacc647182ed1b3f856d051c83a2cb08
lauwen <807444104@qq.com>
2019-06-17 18:26:08 +0800

    The progress bar pointer started

---
+++ js/leo-audio.js
@@ -0,0 +1,11 @@
+(function ($) {
+    $fn.leoAudio = function () {
+
+    }
+    $fn.leoAudio.default = {
+        id : "#leo-audio",
+        name : "",
+        src : "",
+        volume : 0.5
+    }
+})(window.JQuery)
